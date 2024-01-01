8eea5870d68ac379b1dd6e35a1c691602b774e1a
ILS07 <bwheeler@wheelertechinc.com>
2024-01-01 15:33:19 -0600

    Update SQL table and sproc items for BULKDATA

--- Public/Add-SECFilingEntry.ps1
+++ Public/Add-SECFilingEntry.ps1
@@ -12,11 +12,11 @@ Function Add-SECFilingEntry
         foreach ($x in (Get-SECFilingIndex -StartDate ([DateTime]((Invoke-Sqlcmd -Database BULKDATA `
             -Query "SELECT MAX(DateFiled) FROM [dbo].[SEC_FILING_INDEX]").Column1)).AddDays(1)))
         {
-            Invoke-Sqlcmd -Database BULKDATA -Query  ("EXEC [dbo].[Add_SEC_Filing]
-            @dateFiled = '$($x.DateFiled)',
-            @cik = '$($x.CIK.PadLeft(10,'0'))',
-            @formType = '$($x.FormType.Trim())',
-            @fileName = '$($x.FileName)'")
+            Invoke-Sqlcmd -Database BULKDATA -Query ("EXEC [dbo].[Add_SEC_Filing]
+                @dateFiled = '$($x.DateFiled)',
+                @cik = '$($x.CIK.PadLeft(10,'0'))',
+                @formType = '$($x.FormType.Trim())',
+                @fileName = '$($x.FileName)'")
         }
     }
 }
